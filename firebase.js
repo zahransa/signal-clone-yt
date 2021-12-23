1c4814618a48b23d531ab98b898da3e880d06db8
SaeedZahran <saeedzahran@hotmail.com>
2021-12-23 01:13:59 +0100

    navigation + firebase

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyARl0LkfhUoQ-yhlTeevplPy5BaguPh0V0",
+    authDomain: "signal-clone-yt-build-5255f.firebaseapp.com",
+    projectId: "signal-clone-yt-build-5255f",
+    storageBucket: "signal-clone-yt-build-5255f.appspot.com",
+    messagingSenderId: "1091022127295",
+    appId: "1:1091022127295:web:d4b13da33851851665860a"
+  };
